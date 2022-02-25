5e4f46b05c1cf3e7b15ec0f5063c3c3418a7823b
adolfovidal <adolfosvidal@gmail.com>
2022-02-25 12:54:42 -0300

    update aula 02

--- exercicios/ex001/main.js
+++ exercicios/ex001/main.js
@@ -1,6 +1,36 @@
-if () {
-    var message = 'Hello World'
-}
+// string
+
+const firstName = 'Adolfo';
+const lastName = 'Vidal';
+
+const names = `Paula,Vanessa,Tarsila`
+
+console.log(`Meu nome é ` + firstName + ` ` + lastName); 
+console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
+
+console.log(names.split(",")) // Cortar listas
+
+// numbers
+
+const number = 5;
+
+console.log(number.toString()) // Converter para string
+console.log(typeof number.toString()) // Conferir tipos
 
 
-alert(message)
+// Booleanos
+false
+true
+
+console.log(2 == 2)
+
+
+//null & undefined
+
+const x = null; // variável vazia
+
+const y = undefined; // variável indefinida
+
+
+const list = [1, 2, 3,  4, 5];
+const object = {name: 'Adolfo'}
